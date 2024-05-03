15aeea35a8b5509c3b606768b971e4dcc6919f5d
Mohamed Elashri <mail@elashri.com>
2024-05-03 18:34:48 +0000

    Fix url in tags

--- free-mac.rb
+++ free-mac.rb
@@ -1,7 +1,7 @@
 class FreeMac < Formula
   desc "A free-like tool for macOS"
   homepage "https://github.com/MohamedElashri/free-mac"
-  url "https://github.com/MohamedElashri/free-mac/archive/refs/tags/v0.2.0.tar.gz"
+  url "https://github.com/MohamedElashri/free-mac/archive/refs/tags/v0.2.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
